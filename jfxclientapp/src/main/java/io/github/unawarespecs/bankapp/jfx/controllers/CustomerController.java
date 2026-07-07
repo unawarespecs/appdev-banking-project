d57c9459c30b7a11405494168907b9ad35b33c6e
zeelowIDK <bmarasigan1228@gmail.com>
2026-07-07 15:01:57 +0800

    added withdraw and deposit
    fixed error width and css
    
    changed customer controller

--- jfxclientapp/src/main/java/io/github/unawarespecs/bankapp/jfx/controllers/CustomerController.java
+++ jfxclientapp/src/main/java/io/github/unawarespecs/bankapp/jfx/controllers/CustomerController.java
@@ -2,21 +2,38 @@ package io.github.unawarespecs.bankapp.jfx.controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class CustomerController {
 
+    // --- Sidebar or Dashboard Labels ---
     @FXML private Label balanceLabel;
     @FXML private TextField amountInput;
 
+    // --- Deposit View Fields ---
+    @FXML private TextField depositAmountField;
+    @FXML private Button confirmDepositButton;
+    @FXML private Label depositStatusLabel;
+
+    // --- Withdraw View Fields ---
+    @FXML private TextField withdrawAmountField;
+    @FXML private Button confirmWithdrawButton;
+    @FXML private Label withdrawStatusLabel;
+
+    // --- Action Methods matching your FXML ---
     @FXML
-    private void handleDeposit(ActionEvent event) {
-        balanceLabel.setText("Deposit Button Clicked");
+    private void handleDepositSubmit(ActionEvent event) {
+        if (depositStatusLabel != null) {
+            depositStatusLabel.setText("Deposit of $" + depositAmountField.getText() + " submitted!");
+        }
     }
 
     @FXML
-    private void handleWithdraw(ActionEvent event) {
-        balanceLabel.setText("Withdraw Button Clicked");
+    private void handleWithdrawSubmit(ActionEvent event) {
+        if (withdrawStatusLabel != null) {
+            withdrawStatusLabel.setText("Withdrawal of $" + withdrawAmountField.getText() + " submitted!");
+        }
     }
 }
